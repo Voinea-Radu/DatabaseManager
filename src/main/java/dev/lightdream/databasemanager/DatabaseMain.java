0d907de312074dd88829b9bdf3bbb54dd7e21212
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 03:53:47 +0200

    Added version print-out

--- src/main/java/dev/lightdream/databasemanager/DatabaseMain.java
+++ src/main/java/dev/lightdream/databasemanager/DatabaseMain.java
@@ -3,10 +3,20 @@ package dev.lightdream.databasemanager;
 import dev.lightdream.databasemanager.database.IDatabaseManager;
 import dev.lightdream.databasemanager.dto.DriverConfig;
 import dev.lightdream.databasemanager.dto.SQLConfig;
+import dev.lightdream.lambda.LambdaExecutor;
+import dev.lightdream.logger.LoggableMain;
 
+import javax.xml.crypto.Data;
 import java.io.File;
 
 public interface DatabaseMain {
+
+    @SuppressWarnings("unused")
+    static String getVersion() {
+        return "DatabaseManager " + DatabaseMain.class.getPackage().getImplementationVersion() +
+                "    -> " + LoggableMain.getVersion()+
+                "    -> " + LambdaExecutor.getVersion();
+    }
 
     File getDataFolder();
 
