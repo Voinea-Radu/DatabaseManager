66fd8385257de4928b79c089b8e130c47ad36a7f
L1ghtDream <radu.voinea.mihai@gmail.com>
2022-03-18 04:13:38 +0200

    Fixed version

--- src/main/java/dev/lightdream/databasemanager/DatabaseMain.java
+++ src/main/java/dev/lightdream/databasemanager/DatabaseMain.java
@@ -6,16 +6,23 @@ import dev.lightdream.databasemanager.dto.SQLConfig;
 import dev.lightdream.lambda.LambdaExecutor;
 import dev.lightdream.logger.LoggableMain;
 
-import javax.xml.crypto.Data;
 import java.io.File;
 
 public interface DatabaseMain {
 
-    @SuppressWarnings("unused")
-    static String getVersion() {
-        return "DatabaseManager " + DatabaseMain.class.getPackage().getImplementationVersion() +
-                "    -> " + LoggableMain.getVersion()+
-                "    -> " + LambdaExecutor.getVersion();
+    @SuppressWarnings({"unused", "StringConcatenationInLoop"})
+    static String getVersion(int tabs) {
+        String output = "DatabaseManager 2.6.38";
+
+        String prepend = "";
+
+        for (int i = 0; i < tabs; ++i) {
+            prepend += "    ";
+        }
+
+        output = output + prepend + "    -> " + LoggableMain.getVersion() + "\n";
+        output = output + prepend + "    -> " + LambdaExecutor.getVersion() + "\n";
+        return output;
     }
 
     File getDataFolder();
