dc39198347a5bf92a55add3561e68b4bbed54ce0
Radu Voinea <radu.voinea.mihai@gmail.com>
2023-04-07 17:37:23 +0300

    Updated build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("maven-publish")
 }
 
 group = "dev.lightdream"
@@ -42,4 +43,47 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
     targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        val githubURL = project.findProperty("github.url") ?: ""
+        val githubUsername = project.findProperty("github.auth.username") ?: ""
+        val githubPassword = project.findProperty("github.auth.password") ?: ""
+
+        val selfURL = project.findProperty("self.url") ?: ""
+        val selfUsername = project.findProperty("self.auth.username") ?: ""
+        val selfPassword = project.findProperty("self.auth.password") ?: ""
+
+        maven(url = githubURL as String) {
+            name = "github"
+            credentials(PasswordCredentials::class) {
+                username = githubUsername as String
+                password = githubPassword as String
+            }
+        }
+
+        maven(url = selfURL as String) {
+            name = "self"
+            credentials(PasswordCredentials::class) {
+                username = selfUsername as String
+                password = selfPassword as String
+            }
+        }
+    }
+}
+
+tasks.register("publishGitHub") {
+    dependsOn("publishMavenPublicationToGithubRepository")
+    description = "Publishes to GitHub"
+}
+
+tasks.register("publishSelf") {
+    dependsOn("publishMavenPublicationToSelfRepository")
+    description = "Publishes to Self hosted repository"
 }
